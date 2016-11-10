881caba86ec48d05e3a3dd24bbf439659fbae7ba
Serg <serg.zhukovsky@gmail.com>
2016-11-10 14:22:11 -0500

    Added Canvas and WebGL fingerprinting protection
    Auditors: @bbondy
    
    Conflicts:
    	chrome/common/render_messages.h
    	components/content_settings/content/common/content_settings_messages.h
    	third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
    	third_party/WebKit/Source/core/loader/FrameLoaderClient.h
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.h
    	third_party/WebKit/public/web/WebContentSettingsClient.h
    
    Conflicts:
    	chrome/browser/renderer_host/chrome_render_message_filter.cc
    	chrome/browser/renderer_host/chrome_render_message_filter.h
    
    Conflicts:
    	chrome/browser/content_settings/tab_specific_content_settings.cc
    	chrome/browser/renderer_host/chrome_render_message_filter.h
    	chrome/renderer/content_settings_observer.h
    	third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
    	third_party/WebKit/public/web/WebContentSettingsClient.h

--- chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
@@ -252,6 +252,7 @@ public class Tab
     private int mAdsAndTrackers;
     private int mHttpsUpgrades;
     private int mScriptsBlocked;
+    private int mFingerprintsBlocked;
 
     private TabDelegateFactory mDelegateFactory;
 
@@ -344,6 +345,7 @@ public class Tab
         mAdsAndTrackers = 0;
         mHttpsUpgrades = 0;
         mScriptsBlocked = 0;
+        mFingerprintsBlocked = 0;
     }
 
     /**
@@ -1950,10 +1952,11 @@ public class Tab
     }
 
     public void braveShieldsCountUpdate(int adsAndTrackers, int httpsUpgrades
-            , int scriptsBlocked) {
+            , int scriptsBlocked, int fingerprintsBlocked) {
         mAdsAndTrackers += adsAndTrackers;
         mHttpsUpgrades += httpsUpgrades;
         mScriptsBlocked += scriptsBlocked;
+        mFingerprintsBlocked += fingerprintsBlocked;
     }
 
     public int getAdsAndTrackers() {
@@ -1968,10 +1971,15 @@ public class Tab
         return mScriptsBlocked;
     }
 
+    public int getFingerprintsBlocked() {
+        return mFingerprintsBlocked;
+    }
+
     public void clearBraveShieldsCount() {
         mAdsAndTrackers = 0;
         mHttpsUpgrades = 0;
         mScriptsBlocked = 0;
+        mFingerprintsBlocked = 0;
     }
 
     private native void nativeInit();
