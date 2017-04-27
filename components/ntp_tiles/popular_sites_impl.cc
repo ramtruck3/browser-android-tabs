19ece8f12bf7a3c2708a6247c9f5de4233eec18f
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-04-27 09:59:46 -0400

    Made it buildable after merge

--- components/ntp_tiles/popular_sites_impl.cc
+++ components/ntp_tiles/popular_sites_impl.cc
@@ -208,7 +208,7 @@ void SetDefaultResourceForSite(int index,
 #endif
 
 // Creates the list of popular sites based on a snapshot available for mobile.
-base::Value DefaultPopularSites() {
+/*base::Value DefaultPopularSites() {
 #if !defined(OS_ANDROID) && !defined(OS_IOS)
   return base::Value(base::Value::Type::LIST);
 #else
@@ -236,7 +236,7 @@ base::Value DefaultPopularSites() {
 #endif  // GOOGLE_CHROME_BUILD
   return base::Value::FromUniquePtrValue(std::move(sites));
 #endif  // OS_ANDROID || OS_IOS
-}
+}*/
 
 }  // namespace
 
@@ -414,8 +414,8 @@ void PopularSitesImpl::RegisterProfilePrefs(
 
   user_prefs->RegisterInt64Pref(prefs::kPopularSitesLastDownloadPref, 0);
   user_prefs->RegisterStringPref(prefs::kPopularSitesURLPref, std::string());
-  user_prefs->RegisterListPref(prefs::kPopularSitesJsonPref,
-                               DefaultPopularSites());
+  user_prefs->RegisterListPref(prefs::kPopularSitesJsonPref/*,
+                               DefaultPopularSites()*/);
   int version;
   base::StringToInt(kPopularSitesDefaultVersion, &version);
   user_prefs->RegisterIntegerPref(prefs::kPopularSitesVersionPref, version);
