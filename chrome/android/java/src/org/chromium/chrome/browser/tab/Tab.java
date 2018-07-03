6aa76b3371a504b084c78138b273eb59f4027713
samartnik <artem@brave.com>
2018-07-03 17:13:26 +0300

    Separate DSE for standard and private tabs. DDG offer.

--- chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
@@ -34,6 +34,7 @@ import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.ChromeVersionInfo;
 import org.chromium.chrome.browser.IntentHandler;
+import org.chromium.chrome.browser.search_engines.TemplateUrlService;
 import org.chromium.chrome.browser.UrlConstants;
 import org.chromium.chrome.browser.WarmupManager;
 import org.chromium.chrome.browser.WebContentsFactory;
@@ -761,6 +762,7 @@ public class Tab
     public final void show(@TabSelectionType int type) {
         try {
             TraceEvent.begin("Tab.show");
+            TemplateUrlService.getInstance().updateCurrentDSE(isIncognito());
             if (!isHidden()) return;
             // Keep unsetting mIsHidden above loadIfNeeded(), so that we pass correct visibility
             // when spawning WebContents in loadIfNeeded().
