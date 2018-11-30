10c1661d8131ac23c05f304c2c8222536d85cacb
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-11-30 16:38:14 -0500

    JNI link to RewardsService

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -55,6 +55,7 @@ import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.task.PostTask;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.BraveRewardsNativeWorker;
 import org.chromium.chrome.browser.init.StatsUpdater;
 import org.chromium.chrome.browser.IntentHandler.IntentHandlerDelegate;
 import org.chromium.chrome.browser.IntentHandler.TabOpenType;
@@ -316,6 +317,7 @@ public class ChromeTabbedActivity
     private long mIntentHandlingTimeMs;
 
     private boolean mPartnerPageIsLoaded;
+    private BraveRewardsNativeWorker mBraveRewardsNativeWorker;
 
     private final IncognitoTabHost mIncognitoTabHost = new IncognitoTabHost() {
 
@@ -529,6 +531,11 @@ public class ChromeTabbedActivity
         mActivityStopMetrics = new ActivityStopMetrics();
         mMainIntentMetrics = new MainIntentBehaviorMetrics(this);
         mAppIndexingUtil = new AppIndexingUtil();
+    }
+
+    public BraveRewardsNativeWorker getBraveRewardsNativeWorker() {
+        assert mBraveRewardsNativeWorker != null;
+        return mBraveRewardsNativeWorker;
     }
 
     @Override
@@ -1702,6 +1709,7 @@ public class ChromeTabbedActivity
 
         mUndoBarPopupController =
                 new UndoBarController(this, mTabModelSelectorImpl, this::getSnackbarManager);
+        mBraveRewardsNativeWorker = new BraveRewardsNativeWorker();
     }
 
     @Override
@@ -2302,6 +2310,11 @@ public class ChromeTabbedActivity
         }
 
         IncognitoTabHostRegistry.getInstance().unregister(mIncognitoTabHost);
+
+        if (mBraveRewardsNativeWorker != null) {
+            mBraveRewardsNativeWorker.Destroy();
+            mBraveRewardsNativeWorker = null;
+        }
         super.onDestroyInternal();
     }
 
