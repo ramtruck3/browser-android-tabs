28df0471813f443aaf1ab289fa720cc661ef7961
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2019-03-05 23:23:14 -0500

    Hide ac options when ac is off. Fix# 1138

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTablet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTablet.java
@@ -519,6 +519,9 @@ public class ToolbarTablet extends ToolbarLayout
 
     @Override
     public void OnGetRewardsMainEnabled(boolean enabled) {}
+
+    @Override
+    public void OnGetAutoContributeProps() {}
     
 
     /**
