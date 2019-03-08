a8e77fff64458e02618822b49a61bea116fd556c
samartnik <artem@brave.com>
2019-03-08 16:51:38 -0500

    Reverted dynamic colors for top bar

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 package org.chromium.chrome.browser.toolbar;
-import android.util.Log;
 
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -229,7 +228,7 @@ public class ToolbarManager implements ScrimObserver, ToolbarTabController, UrlF
     private boolean mInitializedWithNative;
     private Runnable mOnInitializedRunnable;
 
-    private boolean mShouldUpdateToolbarPrimaryColor;
+    private boolean mShouldUpdateToolbarPrimaryColor = true;
     private int mCurrentThemeColor;
 
     private OmniboxStartupMetrics mOmniboxStartupMetrics;
