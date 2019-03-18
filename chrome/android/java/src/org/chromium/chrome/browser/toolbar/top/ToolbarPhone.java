efe94882ca2c063bbab9b07fbb9acbb29f6039aa
samartnik <artem@brave.com>
2019-03-18 11:54:59 -0400

    Fixed build for cr-73

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -59,6 +59,7 @@ import org.chromium.chrome.browser.BraveRewardsNativeWorker;
 import org.chromium.chrome.browser.BraveRewardsObserver;
 import org.chromium.chrome.browser.BraveRewardsPanelPopup;
 import org.chromium.chrome.browser.ChromeActivity;
+import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.compositor.Invalidator;
 import org.chromium.chrome.browser.compositor.layouts.LayoutUpdateHost;
 import org.chromium.chrome.browser.device.DeviceClassManager;
@@ -487,6 +488,7 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
                 } else {
                     return getCurrentTabView();
                 }
+            }
 
             @Override
             public View getNextFocusBackward() {
@@ -494,7 +496,6 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
             }
         });
         mBraveShieldsButton.setOnClickListener(this);
-        mBraveShieldsButton.setOnLongClickListener(this);
         mBraveRewardsPanelButton.setOnClickListener(this);
     }
 
@@ -667,7 +668,7 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
 
         // Chrome prevents layout_gravity="left" from being defined in XML, but it simplifies
         // the logic, so it is manually specified here.
-        locationBarLayoutParams.gravity = Gravity.TOP | Gravity.LEFT;
+        locationBarLayoutParams.gravity = Gravity.TOP | Gravity.START;
 
         int width = 0;
         int leftMargin = 0;
--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -941,7 +941,7 @@ std::string ContentBrowserClient::GetProduct() const {
   return std::string();
 }
 
-std::string ContentBrowserClient::GetUserAgent() const {
+std::string ContentBrowserClient::GetUserAgent(const std::string& host) const {
   return std::string();
 }
 
