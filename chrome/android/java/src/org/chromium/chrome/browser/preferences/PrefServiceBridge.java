755b6e7e658cacf288eeda2fe67e07890ad3bb47
samartnik <artem@brave.com>
2019-04-02 17:37:07 -0400

    Added initial check for SafetyNet check result

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
@@ -1217,6 +1217,17 @@ public class PrefServiceBridge {
         sInstance = instanceForTesting;
     }
 
+    /**
+     * @param whether SafetyNet check failed.
+     */
+    public void setSafetynetCheckFailed(boolean value) {
+        nativeSetSafetynetCheckFailed(value);
+    }
+
+    public boolean isSafetynetCheckFailed() {
+        return nativeGetSafetynetCheckFailed();
+    }
+
     private native boolean nativeGetBoolean(int preference);
     private native void nativeSetBoolean(int preference, boolean value);
     private native boolean nativeGetAcceptCookiesEnabled();
@@ -1352,4 +1363,6 @@ public class PrefServiceBridge {
     private native void nativeSetPromptForDownloadAndroid(int status);
     private native boolean nativeGetExplicitLanguageAskPromptShown();
     private native void nativeSetExplicitLanguageAskPromptShown(boolean shown);
+    private native void nativeSetSafetynetCheckFailed(boolean value);
+    private native boolean nativeGetSafetynetCheckFailed();
 }
