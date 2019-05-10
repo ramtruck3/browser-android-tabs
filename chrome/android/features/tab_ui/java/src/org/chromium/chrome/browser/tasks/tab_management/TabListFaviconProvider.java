0de3a64688efa8078e97bc5871a655e70739fdc0
samartnik <artem@brave.com>
2019-05-10 15:08:10 -0400

    Applied new New Tab icon

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListFaviconProvider.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListFaviconProvider.java
@@ -49,7 +49,7 @@ public class TabListFaviconProvider {
         }
         if (sRoundedChromeDrawable == null) {
             Bitmap chromeBitmap =
-                    BitmapFactory.decodeResource(context.getResources(), R.drawable.brave_logo_19);
+                    BitmapFactory.decodeResource(context.getResources(), R.drawable.lion);
             sRoundedChromeDrawable = processBitmap(chromeBitmap);
         }
     }
