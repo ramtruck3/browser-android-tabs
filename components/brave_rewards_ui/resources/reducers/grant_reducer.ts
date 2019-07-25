1bfb20a5194eb349db80fa14477f645e393970c3
ryanml <rlanese@asu.edu>
2019-07-25 12:14:46 -0700

    Adding default grant args

--- components/brave_rewards_ui/resources/reducers/grant_reducer.ts
+++ components/brave_rewards_ui/resources/reducers/grant_reducer.ts
@@ -33,7 +33,7 @@ const grantTypeMap = {
 const grantReducer: Reducer<Rewards.State | undefined> = (state: Rewards.State, action) => {
   switch (action.type) {
     case types.GET_GRANTS:
-      chrome.send('brave_rewards.getGrants', [])
+      chrome.send('brave_rewards.getGrants', ['', ''])
       break
     case types.ON_GRANT:
       state = { ...state }
